06453649d2b9d3d0be6aefaa514caa7982ac7c6e
mariastlouis <msantray@yahoo.com>
2017-11-04 12:49:39 -0600

    additional card tests

--- test/Card.test.js
+++ test/Card.test.js
@@ -7,49 +7,62 @@ const fillerData = {
   cond: 'Partly Cloudy',
   temp: '56°F',
   high: '65°F',
-  low: '32°F'
+  low: '32°F',
+  img: 'http://icons.wxug.com/i/c/k/clear.gif'
 }
 
 
-describe('Card', () => {
-  let wrapper;
 
-  beforeEach(() => {
-    wrapper = shallow(<Card />)
-  })
+describe('Card', () => {
+  let card;
+
+    beforeEach(() => {
+    card = shallow(<Card
+      cond = {fillerData.cond}
+      high={fillerData.high}
+      low={fillerData.low}
+      time={fillerData.time}
+      img={fillerData.img}
+      temp = {fillerData.temp}
+    />);
+  });
+
 
   it('Card should exist', () => {
-    expect(wrapper).toBeDefined()
-    console.log(wrapper.debug());
+    expect(card).toBeDefined()
+    console.log(card.debug());
   })
 
    it('Should should render passed-in time', () => {
-    const card = shallow( <Card time={fillerData.time} />);
     expect( card.find('.time').length ).toEqual(1);
     expect('7:00 AM').toEqual(fillerData.time);
   })
 
  it('Should should render passed-in condition', () => {
-    const card = shallow( <Card cond = {fillerData.cond} />);
     expect( card.find('.cond').length ).toEqual(1);
    expect( card.find('.cond').text() ).toEqual('Partly Cloudy');
 
   })
 
  
-  it('Should render a temp div', () => {
-    const card = shallow( <Card temp={fillerData.temp} />);
+  it('Should render a passed-in temp', () => {
     expect( card.find('.temp').length ).toEqual(1);
     expect('56°F').toEqual(fillerData.temp);
   })
 
   it('Should be able to render a high and low', () => {
-    const card = shallow( <Card high={fillerData.high} low={fillerData.low} />);
     expect( card.find('.temp').length ).toEqual(1);      
     expect("65°F").toEqual(fillerData.high);
     expect("32°F").toEqual(fillerData.low);
   })
 
- 
-
+  it('should render an image displaying the current weather', () => {
+    expect(card.find('.card-img')).toBeDefined();
   });
+
+
+})
+
+
+
+
