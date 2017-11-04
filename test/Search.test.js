b9cbeb93a73ecafa3a36cbbbea3f267f53c9d9bb
Julie Dursema <jmdursema@gmail.com>
2017-11-04 11:55:10 -0600

    removed body tag from welcome

--- test/Search.test.js
+++ test/Search.test.js
@@ -2,19 +2,42 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import Search from '../lib/Search'
 
-describe('Card', () => {
+describe('Search', () => {
     it('should start with an value array in state with nothing in it', () => {
       const search = shallow(<Search />);
   
       expect( search.state('value') ).toEqual([]);
     });
 
-    it('should change the value to the input when button is clicked', () => {
+    it('should change the value to the input is changed', () => {
         const search = shallow(<Search />);
+        const input = search.find('input');
         
-        expect( search.state('value') ).toEqual([]);
-    
-        console.log(search)
+        const mockEvent = {
+            target: {
+              value: 'Portland'
+            }
+          }
+      
 
+        expect( search.state('value')[0] ).toEqual();
+
+        input.simulate('change', mockEvent);
+
+        expect( search.state('value') ).toEqual('Portland');
     });
+
+    it('should make suggestions based on the input', () =>{
+        const search = shallow(<Search />);
+        const input = search.find('input');
+
+        const mockEvent = {
+            target: {
+              value: 'Portland'
+            }
+          }
+          input.simulate('change', mockEvent);
+
+          expect( search.updateInputValue.suggestions ).toEqual([ 'portland, or', 'portland, me' ]);
+    })
 });
