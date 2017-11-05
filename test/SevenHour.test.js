567274abff61b2298f13e5e710358d4798d348d8
mariastlouis <msantray@yahoo.com>
2017-11-04 21:47:12 -0600

    starting seven-hour tests

--- test/SevenHour.test.js
+++ test/SevenHour.test.js
@@ -1,19 +1,44 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import SevenHour from '../lib/SevenHour';
+import testData from '../lib/testData.js';
 
 describe('SevenHour', () => {
-  let card;
+  const sevenHour = testData.hourly_forecast.slice(0, 7).reduce((sevenArray, hour) => {
+    sevenArray.push
+    ({time: hour.FCTTIME.civil, 
+      temp: hour.temp.english, 
+      cond: hour.condition, 
+      img: hour.icon_url})
+    return sevenArray
+  }, [])
 
-  beforeEach(() => {
-    card = shallow(<Card />);
+  let wrapper;
 
-  })
+beforeEach(() => {
+  wrapper = shallow(
+    <SevenHour seven = {sevenHour} />
+    )
+});
 
-    it('Should have a className SevenHour', () => {
-        
-        expect(card.find('.seven-hour').length ).toEqual(1);
-    })
+  it('should exist', () => {
+  expect(wrapper).toBeDefined();
+  console.log(wrapper.debug());
+  });
+
+  it('should have all of its elements inside a class of seven-hour', () => {
+    expect(wrapper.find('.seven-hour').length).toEqual(1)
+  });
+
+  it('should render 7 cards', () => {
+  expect(wrapper.find('Card').length).toEqual(7);
+    });
+
+  
+
+
+   
+
 
 
 });
