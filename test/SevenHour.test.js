5afe5f7ec7009770a4f9195b148d75cc24844b5a
mariastlouis <msantray@yahoo.com>
2017-11-04 22:38:42 -0600

    ten hour tests passsing

--- test/SevenHour.test.js
+++ test/SevenHour.test.js
@@ -2,43 +2,37 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import SevenHour from '../lib/SevenHour';
 import testData from '../lib/testData.js';
+import cleanData from '../lib/cleanData.js'
+
+let mockData = cleanData(testData)
+
 
 describe('SevenHour', () => {
-  const sevenHour = testData.hourly_forecast.slice(0, 7).reduce((sevenArray, hour) => {
-    sevenArray.push
-    ({time: hour.FCTTIME.civil, 
-      temp: hour.temp.english, 
-      cond: hour.condition, 
-      img: hour.icon_url})
-    return sevenArray
-  }, [])
-
   let wrapper;
 
-beforeEach(() => {
-  wrapper = shallow(
-    <SevenHour seven = {sevenHour} />
-    )
-});
-
   it('should exist', () => {
+  wrapper = shallow( <SevenHour seven = {mockData.sevenHour} />)
   expect(wrapper).toBeDefined();
-  console.log(wrapper.debug());
   });
 
   it('should have all of its elements inside a class of seven-hour', () => {
+     wrapper = shallow( <SevenHour seven = {mockData.sevenHour} />)
     expect(wrapper.find('.seven-hour').length).toEqual(1)
   });
 
   it('should render 7 cards', () => {
-  expect(wrapper.find('Card').length).toEqual(7);
-    });
+    wrapper = shallow( <SevenHour seven = {mockData.sevenHour} />)
+    expect(wrapper.find('Card').length).toEqual(7);
+  });
 
-  
+  it('should take in time as a property', () => {
+    wrapper = mount( <SevenHour seven = {mockData.sevenHour} />)
+    expect(wrapper.props().seven[0].time).toEqual('12:00 PM')
+  });
 
-
-   
-
-
+  it('should take in temperature as a property', () => {
+    wrapper = mount( <SevenHour seven = {mockData.sevenHour} />)
+    expect(wrapper.props().seven[0].temp).toEqual('40')
+  });
 
 });
